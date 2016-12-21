6cc60a4bad3fec9fcd5c906ffd8e513eab8f6df5
brekt <speakoftrees@gmail.com>
2016-12-21 13:10:36 -0800

    Getting json data client side. Fixed bad json.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
+const fs = require('fs');
 
 // middleware
+
 app.use(express.static('public'));
 
 // routes
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/views/index.html`);
 });
 
+app.get('/data', (req, res) => {
+  let obj;
+  fs.readFile('./data/playlist.json', 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      res.send(err);
+    } else {
+      obj = JSON.parse(data);
+      res.json(obj);
+    }
+  });
+});
+
 app.listen(3333, () => {
-  console.log('App listening on port 3333.');
+  console.log('Navigate your browser to localhost:3333.');
 });
---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var content = {};
+
+// ajax request to get playlist data
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:3333/data');
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    content = parseData(xhr.responseText);
+    console.log(content.thumbnails);
+  } else {
+    alert('Request failed.  Returned status of ' + xhr.status);
+  }
+};
+xhr.send();
+
+// parse data for content
+function parseData(data) {
+  console.log(data);
+  data = JSON.parse(data);
+  // content.thumbnails = data.map((obj) => {
+  //   let imageUrl = `https://static01.nyt.com/${obj[images][3][url]}`;
+  //   return imageUrl;
+  // });
+  return content;
+}
