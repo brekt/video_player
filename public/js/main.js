8216fad5bc753bd410125ffa10274cebd0b008fa
brekt <speakoftrees@gmail.com>
2016-12-24 16:39:05 -0800

    Seeing problem with scope now. Need to pause and check master.

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ var content = {};
 })();
 
 /**
- * parse data for content
+ * parse data for content.
  * @param {string} data - string from ajax request to be parsed as json.
  * @return {object} - an object holding arrays of content data.
  */
@@ -32,30 +32,87 @@ function parseData(data) {
     var videoUrl = obj['renditions'][1]['url'];
     return videoUrl;
   });
-  content.data = data; // replacing string with parsed json on global obj;
-  return content;
+  return data;
 }
 
 /**
- * add video and thumbnail images to view
+ * add video and thumbnail images to view.
  *
  */
 function initializeDOM() {
   var thumbnailElements = document.querySelectorAll('div.thumbnails');
   var videoElement = document.querySelector('video');
+  var videoTitle = document.querySelector('h1.video-title');
+  var videoDescription = document.querySelector('h3.video-description');
   // set up video thumbnails
-  for (var i = 0; i < thumbnailElements.length; i++) {
+  var currentThumbImgClasses = thumbnailElements[0].querySelector('img').classList;
+  currentThumbImgClasses.add('current-video');
+
+  var _loop = function _loop(i) {
     var imgElement = thumbnailElements[i].querySelector('img');
     imgElement.src = content.thumbnailUrls[i];
+    imgElement.addEventListener('click', function (event) {
+      event.preventDefault();
+      currentThumbImgClasses.remove('current-video');
+      playSpecificVideo(i);
+    });
+  };
+
+  for (var i = 0; i < thumbnailElements.length; i++) {
+    _loop(i);
   }
   // set up video element
   videoElement.src = content.videoUrls[0];
   content.currentVideo = 0;
   videoElement.addEventListener('ended', function (event) {
     event.preventDefault();
-    content.currentVideo === content.videoUrls.length - 1 ? content.currentVideo = 0 : content.currentVideo++;
-    videoElement.src = content.videoUrls[content.currentVideo];
-    videoElement.load();
-    videoElement.play();
+    currentThumbImgClasses.remove('current-video');
+    playNextVideo(videoElement);
   });
+  // set up video info section
+  videoTitle.innerHTML = content.data[0]['title'];
+  videoDescription.innerHTML = content.data[0]['summary'];
+}
+
+/**
+ * plays next video after previous video ends.
+ * @param {object} videoElement - HTML video element.
+ *
+ */
+function playNextVideo(videoElement) {
+  content.currentVideo === content.videoUrls.length - 1 ? content.currentVideo = 0 : content.currentVideo++;
+  videoElement.src = content.videoUrls[content.currentVideo];
+  videoTitle.innerHTML = content.data[content.currentVideo]['title'];
+  videoDescription.innerHTML = content.data[content.currentVideo]['description'];
+  videoElement.load();
+  videoElement.play();
+  updateThumbnails(content.currentVideo);
+}
+
+/**
+ * plays a specific video on click of thumbnail.
+ * @param {object} videoElement - HTML video element.
+ * @param {number} whichVideo - index of video to play.
+ *
+ */
+function playSpecificVideo(videoElement, whichVideo) {
+  //  videoElement.pause();
+  videoElement.src = content.videoUrls[whichVideo];
+  videoTitle.innerHTML = content.data[whichVideo]['title'];
+  videoDescription.innerHTML = content.data[whichVideo]['description'];
+  videoElement.load();
+  videoElement.play();
+  content.currentVideo = whichVideo;
+  updateThumbnails(whichVideo);
+}
+
+/**
+ * plays a specific video on click of thumbnail.
+ * @param {number} whichVideo - index of video to play.
+ *
+ */
+function updateThumbnails(whichVideo) {
+  var thumbnailElements = document.querySelectorAll('div.thumbnails');
+  var thumbnailImgClasses = thumbnailElements[whichVideo].querySelector('img').classList;
+  thumbnailImgClasses.add('current-video');
 }
--- src/es6/main.es6
+++ src/es6/main.es6
@@ -16,7 +16,7 @@ const content = {};
 })();
 
 /**
- * parse data for content
+ * parse data for content.
  * @param {string} data - string from ajax request to be parsed as json.
  * @return {object} - an object holding arrays of content data.
  */
@@ -30,32 +30,87 @@ function parseData(data) {
     let videoUrl = obj['renditions'][1]['url'];
     return videoUrl;
   });
-  content.data = data; // replacing string with parsed json on global obj;
-  return content;
+  return data;
 }
 
 /**
- * add video and thumbnail images to view
+ * add video and thumbnail images to view.
  *
  */
 function initializeDOM() {
   let thumbnailElements = document.querySelectorAll('div.thumbnails');
   let videoElement = document.querySelector('video');
+  let videoTitle = document.querySelector('h1.video-title');
+  let videoDescription = document.querySelector('h3.video-description');
   // set up video thumbnails
+  let currentThumbImgClasses = thumbnailElements[0]
+    .querySelector('img').classList;
+  currentThumbImgClasses.add('current-video');
   for (let i = 0; i < thumbnailElements.length; i++) {
     let imgElement = thumbnailElements[i].querySelector('img');
     imgElement.src = content.thumbnailUrls[i];
+    imgElement.addEventListener('click', (event) => {
+      event.preventDefault();
+      currentThumbImgClasses.remove('current-video');
+      playSpecificVideo(i);
+    });
   }
   // set up video element
   videoElement.src = content.videoUrls[0];
   content.currentVideo = 0;
   videoElement.addEventListener('ended', (event) => {
     event.preventDefault();
-    content.currentVideo === content.videoUrls.length - 1
-      ? content.currentVideo = 0
-      : content.currentVideo++;
-    videoElement.src = content.videoUrls[content.currentVideo];
-    videoElement.load();
-    videoElement.play();
+    currentThumbImgClasses.remove('current-video');
+    playNextVideo(videoElement);
   });
+  // set up video info section
+  videoTitle.innerHTML = content.data[0]['title'];
+  videoDescription.innerHTML = content.data[0]['summary'];
 }
+
+/**
+ * plays next video after previous video ends.
+ * @param {object} videoElement - HTML video element.
+ *
+ */
+ function playNextVideo(videoElement) {
+   content.currentVideo === content.videoUrls.length - 1
+     ? content.currentVideo = 0
+     : content.currentVideo++;
+   videoElement.src = content.videoUrls[content.currentVideo];
+   videoTitle.innerHTML = content.data[content.currentVideo]['title'];
+   videoDescription.innerHTML =
+    content.data[content.currentVideo]['description'];
+   videoElement.load();
+   videoElement.play();
+   updateThumbnails(content.currentVideo);
+ }
+
+ /**
+  * plays a specific video on click of thumbnail.
+  * @param {object} videoElement - HTML video element.
+  * @param {number} whichVideo - index of video to play.
+  *
+  */
+ function playSpecificVideo(videoElement, whichVideo) {
+  //  videoElement.pause();
+   videoElement.src = content.videoUrls[whichVideo];
+   videoTitle.innerHTML = content.data[whichVideo]['title'];
+   videoDescription.innerHTML = content.data[whichVideo]['description'];
+   videoElement.load();
+   videoElement.play();
+   content.currentVideo = whichVideo;
+   updateThumbnails(whichVideo);
+ }
+
+ /**
+  * plays a specific video on click of thumbnail.
+  * @param {number} whichVideo - index of video to play.
+  *
+  */
+ function updateThumbnails(whichVideo) {
+   let thumbnailElements = document.querySelectorAll('div.thumbnails');
+   let thumbnailImgClasses = thumbnailElements[whichVideo]
+     .querySelector('img').classList;
+   thumbnailImgClasses.add('current-video');
+ }
